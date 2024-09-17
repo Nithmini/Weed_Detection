4a7ad1ada66aa0271dc2f490832e2a9c54c5d1c9
Nithmini Manimelwadu <70828453+Nithmini@users.noreply.github.com>
2024-09-17 21:53:06 +0530

    Footer

---
+++ weed-detection-frontend/src/components/Footer.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import './css/Footer.css';
+import seaLogo from './images/logo.png';
+import { FaInstagram, FaTwitter, FaFacebookF, FaLinkedinIn, FaFacebook } from 'react-icons/fa'; 
+const Footer = () => {
+  return (
+    <footer className="footer">
+      <div className="footer-logo">
+        <img src={seaLogo} alt="Sea Logo" className="logo" />
+      </div>
+      <div className="footer-links">
+        <a href="#home">Home</a>
+        <a href="#about">About</a>
+        <a href="#services">Service</a>
+        <a href="#contact">Contact Us</a>
+      </div>
+      <div className="social-icons">
+        <FaInstagram className="icon" />
+        <FaTwitter className="icon" />
+        <FaFacebookF className="icon" />
+        <FaLinkedinIn className="icon linkedin" />
+        <FaFacebook className="icon" />
+      </div>
+      <hr className="horizontal-line" />
+      <div className="copyright">
+        <p>Copyright green guardian</p>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
